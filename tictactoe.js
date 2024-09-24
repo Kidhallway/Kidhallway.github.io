65b335c61d0066c8c84e84d4163b64d14790f553
Kidhallway <Hallada.kade@stu.jamesirwin.org>
2024-09-24 12:58:24 -0600

    zxcvbnm,.;lokiujyhgfdsxc vbnm,.;lokijuhgfd

---
+++ tictactoe.js
@@ -0,0 +1,35 @@
+
+function player () {
+
+}
+
+function win () {
+    Let
+}
+
+
+
+if aa===ab===ac {
+.win
+}
+if ba===bb===bc {
+.win   
+}
+if ca===cb===cc {
+.win   
+}
+if aa===ba===ca {
+.win   
+}
+if ba===bb===bc {
+.win    
+}
+if ca===cb===cc {
+.win   
+}
+if aa===bb===cc {
+.win   
+}
+if ca===bb===ac {
+.win   
+}
