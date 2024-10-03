899848bb817c00dfdda005bf5d792f33f8c694dd
Kidhallway <Hallada.kade@stu.jamesirwin.org>
2024-10-03 09:49:54 -0600

    asdfghjkl;

---
+++ chimpcon.js
@@ -0,0 +1,32 @@
+function detect(a, b) {
+    return Math.floor(Math.random() * (b - a + 1)) + a
+}
+
+console.log (detect) 
+detect ();
+let X = detect (1, 5)
+console.log (X)
+switch (detect(1, 5)) {
+    default:
+    case2:
+    case3:
+    case4:
+    case5:
+}
+
+function getRandomElementById () {
+  return document.getElementById (getrandomitem(...ids));
+}
+
+function getRandomItem (items) {
+    return items [Math.floor(math.random() * items.length)];
+}
+let randomElemnt = getRandomElementById (
+    "chimp5"
+    "chimp4"
+    "chimp3"
+    "chimp2"
+    "chimp1"
+);
+randomElemnt.style.color="red"
+function
